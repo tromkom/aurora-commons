0a497441b3e8961321106f73b8f61d5bc19d0e88
Kasea <>
2019-11-13 11:20:55 +0100

    build

--- dist/index.js
+++ dist/index.js
@@ -15,3 +15,15 @@ Object.keys(_Memoize).forEach(function (key) {
     }
   });
 });
+
+var _utils = require("./utils");
+
+Object.keys(_utils).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _utils[key];
+    }
+  });
+});
